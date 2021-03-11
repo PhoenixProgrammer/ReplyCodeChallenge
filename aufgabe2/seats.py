14385555238bb6e7be82f51233f9a39b2cbbf610
Mika Hoppe <mika.hoppe@gmx.de>
2021-03-11 19:42:17 +0100

    seats solution

--- aufgabe2/seats.py
+++ aufgabe2/seats.py
@@ -1,6 +1,4 @@
 #!/usr/bin/env python3
-
-import sys
 from os.path import dirname, join
 
 # Parsing all the input data 
@@ -11,39 +9,35 @@ def parse_testcases(io):
 
     # For each test case
     for _ in range(T):
-        # Read the input data
-        (a, b) = map(int, io.readline(  ).strip().split())
-        # Add to the list fo test cases
-        tc.append((a,b))
+        (N, k) = map(int, io.readline().strip().split())
+        input_two = list(map(int, io.readline().strip().split()))
+        tc.append([(N, k), input_two])
 
-    # Return all the test cases parsed
     return tc
 
 # Function to solve a single test case
 def solve_testcase(t):
-    # Get data from the test case
-    (a, b) = t
+    (N, k) = t[0]
+    permutation = t[1]
 
-    # Evaluate the solution
-    solution = a+b
-    
-    # Returning the solution
-    return solution
+    positions = list(range(N))
+    for _ in range(k):
+        positions = [positions[i] for i in permutation]
+
+    return ' '.join(map(str, positions))
 
 # Solve 
 def solve(input, output):
     # Enumerate all the test cases
     for i, t in enumerate(parse_testcases(input)):
         # Print the solution of the test case
+        # print(f"Case #{i+1}:", solve_testcase(t), file=output)
+        print(i)
         print(f"Case #{i+1}:", solve_testcase(t), file=output)
 
 current_dir = dirname(__file__)
 
 if __name__ == "__main__":
-    # fin = sys.stdin
-    # fout = sys.stdout
-    # If you want to read and write from files use these two lines:
-    fin = open(join(current_dir, "./input.txt"), "r")
+    fin = open(join(current_dir, "./input-seats-b574.txt"), "r")
     fout = open(join(current_dir, "./output.txt"), "w")
-    print("Read")
     solve(fin, fout)
